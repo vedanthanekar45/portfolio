434ba1155a0ed09b5d948a33108d9b647f2a5bcb
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-02-09 16:27:19 +0530

    feat: homepage completed!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import style from './home.module.css'
+import { inter, bentham } from '@/ui/fonts'
+import { GiFallingStar } from "react-icons/gi";
+import { BsGithub } from "react-icons/bs";
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <body className={style.body}>
-      <div className="w-[1902px] h-[1050px] bg-black opacity-80"></div>
-    </body>
-  );
+    return (
+        <body className={style.body}>
+            
+            {/* Navbar Div */}
+            <div className={`${bentham.className} flex ml-10 font-bold `}>
+                <Link href="/" className='flex'>
+                    <GiFallingStar className="text-white text-5xl mt-4 mr-4" />
+                    <p className="mt-7 text-4xl text-white">
+                        StarStruck
+                    </p>
+                </Link>
+                <Link href="https://github.com/vedanthanekar45/starstruck">
+                    <BsGithub className="text-white text-4xl mt-8 ml-[1550px] opacity-50 hover:opacity-100" />
+                </Link>
+            </div>
+
+            {/* Body Div */}  
+            <div className={`${inter.className} flex flex-col items-center 
+            justify-center text-7xl text-white font-black h-[75vh]`}>
+                <h1 className="text-center">Where every star holds a story, <br></br> waiting to be found</h1>
+            </div>
+        </body>
+    );
 }
--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { Roboto } from 'next/font/google';
+import { Bentham } from 'next/font/google';
 
 export const inter = Inter({
     subsets: ['latin']
@@ -7,4 +8,8 @@ export const inter = Inter({
 
 export const roboto = Roboto({
     weight: '700'
+});
+
+export const bentham = Bentham({
+    weight: '400'
 });
