6f24f55a94ba9460008950ff83cee799a8ce7631
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-06-21 23:29:34 +0530

    fix: ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "@/components/ui/provider"
+// import { Provider } from "@/components/ui/provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Provider>
+        {/* <Provider> */}
             {children}  
-        </Provider>
+        {/* </Provider> */}
       </body>
     </html>
   );
