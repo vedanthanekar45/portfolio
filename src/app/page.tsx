8ed6b35e39ccf66155fb7092f5ae40cb47b8855a
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-07-14 09:31:45 +0530

    chore: greetings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 'use client';
 import style from './home.module.css'
-import { inter, bentham } from '@/ui/fonts'
-import { BsGithub } from "react-icons/bs";
-import Link from 'next/link'
+import { inter } from '@/ui/fonts'
+import { useEffect, useState } from "react";
+// import { BsGithub } from "react-icons/bs";
+// import Link from 'next/link'
 import { greetings } from '@/ui/hello';
 
 export default function Home() {
 
+    const [idx, setIdx] = useState(0);
 
+    useEffect(() => {
+    const interval = setInterval(() => {
+      setIdx((prevIndex) => {
+        return (prevIndex + 1) % greetings.length;
+      });
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
 
     return (
         <body className={style.body}>
-            <div className="flex items-center top-36">
-                <h1 className={`${inter.className} font-bold text-2xl`}>
-                    {
-                    
-                    }
+            <div className="flex justify-center mt-36">
+                <h1 className={`${inter.className} font-bold text-4xl text-white`}>
+                    {greetings[idx]}, I'm Vedant Thanekar
                 </h1>
             </div>
         </body>
